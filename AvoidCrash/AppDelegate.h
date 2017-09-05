64c6f5d2bdf8dcd54bb79acc2b8bdb21787a7656
王超9 <wangchao9@le.com>
2017-09-06 07:44:46 +0800

    Initial Commit

---
+++ AvoidCrash/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AvoidCrash
+//
+//  Created by wangchao on 2017/9/6.
+//  Copyright © 2017年 ibestv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
